4feddf4e3611dd7d8db764c4b0d14c26a81e9f29
Brad Fincher <matrixv01@gmail.com>
2017-06-09 22:30:44 -0500

    setHiddenFields use template

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,14 +1,14 @@
-let answer = document.getElementById('answer');
-let attempt = document.getElementById('attempt');
+const answer = document.getElementById('answer');
+const attempt = document.getElementById('attempt');
 
 function guess() {
-  let input = document.getElementById('user-guess');
+  const input = document.getElementById('user-guess');
   // add functionality to guess function here
-
 }
 
-// implement new functions here
 function setHiddenFields() {
-  answer = Math.floor(Math.random() * 10000);
-  return answer;
+  answer.value = Math.floor(Math.random() * 10000).toString();
+  while (answer.value.length < 4) {
+    answer.value = `0${answer.value}`;
+  }
 }
