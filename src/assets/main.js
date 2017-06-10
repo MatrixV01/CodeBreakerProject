f5f5c7527b315ed20d56137c2b293be2e6e3118c
Brad Fincher <matrixv01@gmail.com>
2017-06-09 22:18:25 -0500

    Create setHiddenFields

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,8 +2,13 @@ let answer = document.getElementById('answer');
 let attempt = document.getElementById('attempt');
 
 function guess() {
-    let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+  let input = document.getElementById('user-guess');
+  // add functionality to guess function here
+
 }
 
-//implement new functions here
+// implement new functions here
+function setHiddenFields() {
+  answer = Math.floor(Math.random() * 10000);
+  return answer;
+}
